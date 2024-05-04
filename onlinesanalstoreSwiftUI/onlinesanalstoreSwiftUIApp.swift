f072d0f42b2c36a637adb2162f75320f44a795fa
mhtozbnm <mhtozbnm@gmail.com>
2024-05-04 19:10:03 +0300

    Initial Commit

---
+++ onlinesanalstoreSwiftUI/onlinesanalstoreSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  onlinesanalstoreSwiftUIApp.swift
+//  onlinesanalstoreSwiftUI
+//
+//  Created by Mahmut on 4.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct onlinesanalstoreSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
